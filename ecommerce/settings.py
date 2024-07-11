22861ef342ac50fc6e2f81e7b8ecc7fe2b920c18
tomas <t.gonzalezb@duocuc.cl>
2024-07-11 12:12:56 -0400

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,8 +118,12 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "USER": "root",
+        "PASSWORD": "hBKIEBRhAFGHdDfXASUtVkJZGgLXejDr",
+        "HOST": "roundhouse.proxy.rlwy.net",
+        "PORT": 41152,
     }
 }
 
